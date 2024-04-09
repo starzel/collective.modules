a1abe5622b8669df7fe08ce9ece16c0c7e4bfecd
Steffen Lindner <mail@steffen-lindner.de>
2024-04-09 14:15:55 +0200

    Convert resolveuid url to absolute_urls

--- src/collective/modules/utils.py
+++ src/collective/modules/utils.py
@@ -1,6 +1,8 @@
 from plone import api
 from plone.app.contenttypes.browser.link_redirect_view import NON_RESOLVABLE_URL_SCHEMES
+from plone.app.contenttypes.browser.link_redirect_view import normalize_uid_from_path
 from plone.app.contenttypes.utils import replace_link_variables_by_paths
+from plone.app.uuid.utils import uuidToObject
 from zope.globalrequest import getRequest
 
 
@@ -19,6 +21,19 @@ def link_url(url, context, request=None):
             return url
 
     url = replace_link_variables_by_paths(context, url)
+
+    if "resolveuid" in url:
+        uid, fragment = normalize_uid_from_path(url)
+        obj = uuidToObject(uid)
+        if obj is None:
+            # uid can't resolve, return the url
+            return url
+
+        url = obj.absolute_url()
+        if fragment is not None:
+            url = f"{url}{fragment}"
+        return url
+
     if url.startswith('.'):
         context_state = api.content.get_view('@@plone_context_state', context)
         url = '/'.join([context_state.canonical_object_url(), url])
